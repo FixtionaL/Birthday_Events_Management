9ff20133ecfb7e7e50198a8db5a9cc657864716e
Harsh Kumar <harshng7@gmail.com>
2024-01-31 10:36:30 +0530

    first

---
+++ src/pages/admin.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Admin_Nav from '../components/admin_nav';
+const Admin = () => {
+  return (
+    <>
+        <Admin_Nav></Admin_Nav>
+        {/* <div className="content"> */}
+          {/* <div className="land_nav"> */}
+          {/* </div> */}
+          <h1 className="no_video_text">
+          This is the Admin Page for this application! It contains the Main Navbar and facility to navigate everypage and obtain insights!
+          </h1>
+        {/* </div> */}
+    </>
+  )
+};
+export default Admin
